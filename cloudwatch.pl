bf6d120a06da4d234baf2c628cbc99748028e628
root <root@jumpbox.(none)>
2016-08-06 21:26:24 -0400

    new addiditon to the cloud watch script to accept name of the alarm

--- cloudwatch.pl
+++ cloudwatch.pl
@@ -1,18 +1,14 @@
-
 #!/usr/bin/perl
 
-###Script accepts 2 values to be run non-interactively 'cloudwatch.pl <rds> <dbidetifier>' or 'cloudwatch.pl <ec2> <deviceid>'
+###Script accepts 3 values to be run non-interactively 'cloudwatch.pl <rds> <dbidetifier> <alarm_name>' or 'cloudwatch.pl <ec2> <deviceid> <alarm_name>'
 ###See Readme file for more details
 
 use strict;
 use warnings;
 use Term::Menu;
 
-<<<<<<< HEAD
-my($alrminstanceid,$dbidentifier);
+my($alarm_name,$alrminstanceid,$dbidentifier);
 
-=======
->>>>>>> 65c043cb085847c773edf62ccc1883493cffcb04
 #EC2 thresholds 
 my $DiskReadBytes=60000;
 my $NetworkOut=15000000;
@@ -34,36 +30,33 @@ my $WriteLatency=2.0;
 my $NetworkReceiveThroughput=500000.0;
 my $WriteIOPS=2000.0;
 
-<<<<<<< HEAD
 my $num=$#ARGV + 1;
 
-if($num == 2)
+if($num == 3)
 
 {
-print "$ARGV[1]\n";
-print "$ARGV[0]\n";
-
 my $answers= shift;
-#my $answers= "$ARGV[0]";
 
         if($answers eq "rds")
                 {
                	$dbidentifier=$ARGV[1];
+               	$alarm_name=$ARGV[2];
                 rds();
                 }
                         elsif($answers eq "ec2")
 
                         {
-                         $alrminstanceid=$ARGV[1];
-                         ec2();
+                        $alrminstanceid=$ARGV[1];
+						$alarm_name=$ARGV[2];
+						ec2();
                         }
 				else
 				{
-				print"$0 accepts 2 values either (rds or ec2)\n";
+				print"$0 accepts 3 values either (<rds> <dbidentifier> or <ec2> <dbidentifier>)\n";
 				exit;
 				}
 }
-        elsif($num != 2)
+        elsif($num != 3)
             	{
 
                 goto MENU;
@@ -71,8 +64,6 @@ my $answers= shift;
                         }
 
 MENU:
-=======
->>>>>>> 65c043cb085847c773edf62ccc1883493cffcb04
 my $menu = new Term::Menu;
 my $answer = $menu->menu(
       ec2    => ["Setup CloudWatch metrics and alarm for EC2", '1'],
@@ -82,11 +73,19 @@ my $answer = $menu->menu(
 
 if($answer =~ /ec2/mi) 
 {
-     ec2();
+	print"Enter the EC2 InstanceID...e.g(i-8c1fdf18)\n";
+	chomp($alrminstanceid=<>);
+		print"Enter then EC2 Alarm name\n";
+		chomp($alarm_name=<>);
+			ec2();
 } 
 	elsif($answer =~ /rds/mi) 
 	{
-	 rds();
+	print"Enter the RDS DBInstanceIdentifier...e.g(icuc-test-rds-1)\n";
+	chomp(my $dbidentifier=<>);
+		print"Enter the RDS Alarm name ";
+		chomp($$alarm_name=<>);
+			rds();
 	}
 		elsif($answer =~ /exit/mi)
 		{
@@ -95,20 +94,15 @@ if($answer =~ /ec2/mi)
 
 
 sub ec2 {
-
-print"Enter the EC2 InstanceID...e.g(i-8c1fdf18)\n";
-
-chomp($alrminstanceid=<>);
-
-system qq{aws cloudwatch put-metric-alarm --alarm-name $alrminstanceid-DiskReadBytes --alarm-description "Monitor network Disk activity " --metric-name DiskReadBytes --namespace AWS/EC2 --statistic Average --period 300 --threshold $DiskReadBytes --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=InstanceId,Value=$alrminstanceid" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
+system qq{aws cloudwatch put-metric-alarm --alarm-name $alarm_name-DiskReadBytes --alarm-description "Monitor network Disk activity " --metric-name DiskReadBytes --namespace AWS/EC2 --statistic Average --period 300 --threshold $DiskReadBytes --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=InstanceId,Value=$alrminstanceid" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
 sleep(5);
-system qq{aws cloudwatch put-metric-alarm --alarm-name $alrminstanceid-NetworkOut --alarm-description "Monitor network outgoing traffic threshold " --metric-name NetworkOut --namespace AWS/EC2 --statistic Average --period 300 --threshold $NetworkOut --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=InstanceId,Value=$alrminstanceid" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
+system qq{aws cloudwatch put-metric-alarm --alarm-name $alarm_name-NetworkOut --alarm-description "Monitor network outgoing traffic threshold " --metric-name NetworkOut --namespace AWS/EC2 --statistic Average --period 300 --threshold $NetworkOut --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=InstanceId,Value=$alrminstanceid" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
 sleep(5);
-system qq{aws cloudwatch put-metric-alarm --alarm-name $alrminstanceid-DiskReadOps --alarm-description "Monitor DiskReadOps threshold " --metric-name DiskReadOps --namespace AWS/EC2 --statistic Average --period 300 --threshold $DiskReadOps --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=InstanceId,Value=$alrminstanceid" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
+system qq{aws cloudwatch put-metric-alarm --alarm-name $alarm_name-DiskReadOps --alarm-description "Monitor DiskReadOps threshold " --metric-name DiskReadOps --namespace AWS/EC2 --statistic Average --period 300 --threshold $DiskReadOps --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=InstanceId,Value=$alrminstanceid" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
 sleep(5);
-system qq{aws cloudwatch put-metric-alarm --alarm-name $alrminstanceid-NetworkIn --alarm-description "Monitor network incoming traffic threshold " --metric-name NetworkIn --namespace AWS/EC2 --statistic Average --period 300 --threshold $NetworkIn --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=InstanceId,Value=$alrminstanceid" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
+system qq{aws cloudwatch put-metric-alarm --alarm-name $alarm_name-NetworkIn --alarm-description "Monitor network incoming traffic threshold " --metric-name NetworkIn --namespace AWS/EC2 --statistic Average --period 300 --threshold $NetworkIn --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=InstanceId,Value=$alrminstanceid" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
 sleep(5);
-system qq{aws cloudwatch put-metric-alarm --alarm-name $alrminstanceid-CPUUtilization --alarm-description "Monitor EC2 servers cpu" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 300 --threshold $CPUUtilization --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=InstanceId,Value=$alrminstanceid" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert --unit Percent};
+system qq{aws cloudwatch put-metric-alarm --alarm-name $alarm_name-CPUUtilization --alarm-description "Monitor EC2 servers cpu" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 300 --threshold $CPUUtilization --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=InstanceId,Value=$alrminstanceid" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert --unit Percent};
 sleep(2);
 	print "Successfully Configured the following EC2 metrics:\n
 	CPUUtilization\n
@@ -122,36 +116,31 @@ sleep(2);
 
 sub rds {
 
-print"Enter the RDS DBInstanceIdentifier...e.g(icuc-test-rds-1)\n";
 
-<<<<<<< HEAD
-chomp($dbidentifier=<>);
-=======
-chomp(my $dbidentifier=<>);
->>>>>>> 65c043cb085847c773edf62ccc1883493cffcb04
-system qq{aws cloudwatch put-metric-alarm --alarm-name $dbidentifier-ReadLatency --alarm-description "Monitor ReadLatency for RDS " --metric-name ReadLatency --namespace AWS/RDS --statistic Average --period 300 --threshold $ReadLatency --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
+
+system qq{aws cloudwatch put-metric-alarm --alarm-name $alarm_name-ReadLatency --alarm-description "Monitor ReadLatency for RDS " --metric-name ReadLatency --namespace AWS/RDS --statistic Average --period 300 --threshold $ReadLatency --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
 sleep(5);
-system qq{aws cloudwatch put-metric-alarm --alarm-name $dbidentifier-DatabaseConnections --alarm-description "Monitor DatabaseConnections for RDS " --metric-name DatabaseConnections --namespace AWS/RDS --statistic Average --period 300 --threshold $DatabaseConnections --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
+system qq{aws cloudwatch put-metric-alarm --alarm-name $alarm_name-DatabaseConnections --alarm-description "Monitor DatabaseConnections for RDS " --metric-name DatabaseConnections --namespace AWS/RDS --statistic Average --period 300 --threshold $DatabaseConnections --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
 sleep(5);
-system qq{aws cloudwatch put-metric-alarm --alarm-name $dbidentifier-NetworkTransmitThroughput --alarm-description "Monitor NetworkTransmitThroughput for RDS " --metric-name NetworkTransmitThroughput --namespace AWS/RDS --statistic Average --period 300 --threshold $NetworkTransmitThroughput --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
+system qq{aws cloudwatch put-metric-alarm --alarm-name $alarm_name-NetworkTransmitThroughput --alarm-description "Monitor NetworkTransmitThroughput for RDS " --metric-name NetworkTransmitThroughput --namespace AWS/RDS --statistic Average --period 300 --threshold $NetworkTransmitThroughput --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
 sleep(5);
-system qq{aws cloudwatch put-metric-alarm --alarm-name $dbidentifier-WriteThroughput --alarm-description "Monitor WriteThroughput for RDS " --metric-name WriteThroughput --namespace AWS/RDS --statistic Average --period 300 --threshold  $WriteThroughput --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
+system qq{aws cloudwatch put-metric-alarm --alarm-name $alarm_name-WriteThroughput --alarm-description "Monitor WriteThroughput for RDS " --metric-name WriteThroughput --namespace AWS/RDS --statistic Average --period 300 --threshold  $WriteThroughput --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
 sleep(5);
-system qq{aws cloudwatch put-metric-alarm --alarm-name $dbidentifier-ReadIOPS --alarm-description "Monitor ReadIOPS for RDS " --metric-name ReadIOPS --namespace AWS/RDS --statistic Average --period 300 --threshold $ReadIOPS --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 5 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
+system qq{aws cloudwatch put-metric-alarm --alarm-name $alarm_name-ReadIOPS --alarm-description "Monitor ReadIOPS for RDS " --metric-name ReadIOPS --namespace AWS/RDS --statistic Average --period 300 --threshold $ReadIOPS --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 5 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
 sleep(5);
-system qq{aws cloudwatch put-metric-alarm --alarm-name $dbidentifier-FreeStorageSpace --alarm-description "Monitor FreeStorageSpace for RDS " --metric-name FreeStorageSpace --namespace AWS/RDS --statistic Average --period 300 --threshold $FreeStorageSpace --comparison-operator LessThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 1 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
+system qq{aws cloudwatch put-metric-alarm --alarm-name $alarm_name-FreeStorageSpace --alarm-description "Monitor FreeStorageSpace for RDS " --metric-name FreeStorageSpace --namespace AWS/RDS --statistic Average --period 300 --threshold $FreeStorageSpace --comparison-operator LessThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 1 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
 sleep(5);
-system qq{aws cloudwatch put-metric-alarm --alarm-name $dbidentifier-SwapUsage --alarm-description "Monitor SwapUsage for RDS " --metric-name SwapUsage --namespace AWS/RDS --statistic Average --period 300 --threshold $SwapUsage --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
+system qq{aws cloudwatch put-metric-alarm --alarm-name $alarm_name-SwapUsage --alarm-description "Monitor SwapUsage for RDS " --metric-name SwapUsage --namespace AWS/RDS --statistic Average --period 300 --threshold $SwapUsage --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
 sleep(5);
-system qq{aws cloudwatch put-metric-alarm --alarm-name $dbidentifier-FreeableMemory --alarm-description "Monitor FreeableMemory for RDS " --metric-name FreeableMemory --namespace AWS/RDS --statistic Average --period 300 --threshold $FreeableMemory --comparison-operator LessThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
+system qq{aws cloudwatch put-metric-alarm --alarm-name $alarm_name-FreeableMemory --alarm-description "Monitor FreeableMemory for RDS " --metric-name FreeableMemory --namespace AWS/RDS --statistic Average --period 300 --threshold $FreeableMemory --comparison-operator LessThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
 sleep(5);
-system qq{aws cloudwatch put-metric-alarm --alarm-name $dbidentifier-DiskQueueDepth --alarm-description "Monitor DiskQueueDepth for RDS " --metric-name DiskQueueDepth --namespace AWS/RDS --statistic Average --period 300 --threshold $DiskQueueDepth --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
+system qq{aws cloudwatch put-metric-alarm --alarm-name $alarm_name-DiskQueueDepth --alarm-description "Monitor DiskQueueDepth for RDS " --metric-name DiskQueueDepth --namespace AWS/RDS --statistic Average --period 300 --threshold $DiskQueueDepth --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
 sleep(5);
-system qq{aws cloudwatch put-metric-alarm --alarm-name $dbidentifier-WriteLatency --alarm-description "Monitor WriteLatency for RDS " --metric-name WriteLatency --namespace AWS/RDS --statistic Average --period 300 --threshold $WriteLatency --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
+system qq{aws cloudwatch put-metric-alarm --alarm-name $alarm_name-WriteLatency --alarm-description "Monitor WriteLatency for RDS " --metric-name WriteLatency --namespace AWS/RDS --statistic Average --period 300 --threshold $WriteLatency --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
 sleep(5);
-system qq{aws cloudwatch put-metric-alarm --alarm-name $dbidentifier-NetworkReceiveThroughput --alarm-description "Monitor NetworkReceiveThroughput for RDS " --metric-name NetworkReceiveThroughput --namespace AWS/RDS --statistic Average --period 300 --threshold $NetworkReceiveThroughput --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
+system qq{aws cloudwatch put-metric-alarm --alarm-name $alarm_name-NetworkReceiveThroughput --alarm-description "Monitor NetworkReceiveThroughput for RDS " --metric-name NetworkReceiveThroughput --namespace AWS/RDS --statistic Average --period 300 --threshold $NetworkReceiveThroughput --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
 sleep(5);
-system qq{aws cloudwatch put-metric-alarm --alarm-name $dbidentifier-WriteIOPS --alarm-description "Monitor WriteIOPS for RDS " --metric-name WriteIOPS --namespace AWS/RDS --statistic Average --period 300 --threshold $WriteIOPS --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
+system qq{aws cloudwatch put-metric-alarm --alarm-name $alarm_name-WriteIOPS --alarm-description "Monitor WriteIOPS for RDS " --metric-name WriteIOPS --namespace AWS/RDS --statistic Average --period 300 --threshold $WriteIOPS --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=DBInstanceIdentifier,Value=$dbidentifier" --evaluation-periods 3 --alarm-actions arn:aws:sns:us-west-2:484505395026:TechSupport_Alert};
 sleep(2);
 
 print "Successfully Configured the following EC2 metrics:\n
